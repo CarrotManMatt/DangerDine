2342524bd9aa812ee8b9082481c5ff9b51a8e813
CarrotManMatt <matt@carrotmanmatt.com>
2023-11-04 20:08:10 +0000

    Add dangerdine app URLs to root URL conf

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@
 import django
 from django.contrib import admin
 from django.urls import URLPattern, URLResolver
+from django.views.generic import RedirectView
 
 from core.views import AdminDocsRedirectView
 
@@ -16,5 +17,11 @@ urlpatterns: list[URLResolver | URLPattern] = [
         r"admin/docs/<path:subpath>",
         AdminDocsRedirectView.as_view()
     ),
-    django.urls.path(r"admin/", admin.site.urls)
+    django.urls.path(r"admin/", admin.site.urls),
+    django.urls.path("", django.urls.include("dangerdine.urls")),
+    django.urls.path(
+        "",
+        RedirectView.as_view(pattern_name="dangerdine:home"),
+        name="default"
+    )
 ]
