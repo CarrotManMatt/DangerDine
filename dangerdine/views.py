1bb3a093ed0115a32a2a3f0df231cb7f9f698efe
CarrotManMatt <matt@carrotmanmatt.com>
2023-11-05 09:49:35 +0000

    Store incoming new routes

--- dangerdine/views.py
+++ dangerdine/views.py
@@ -5,14 +5,17 @@ from typing import TYPE_CHECKING, Any
 
 from django import shortcuts
 from django.contrib import auth
+from django.contrib.gis.geos import Point
+from django.contrib.gis.db.models.functions import Distance
 from django.http import HttpRequest, HttpResponseRedirect
 from django.views import View
 from django.views.generic.base import TemplateView
 
 import dangerdine.utils
+from dangerdine.models import LocationRoute, BusinessRatingLocation
 
 if TYPE_CHECKING:
-    from dangerdine.models import LocationRoute, User
+    from dangerdine.models import User
 
 # NOTE: Adding external package functions to the global scope for frequent usage
 get_user_model: Callable[[], "User"] = auth.get_user_model  # type: ignore[assignment]
@@ -26,7 +29,13 @@ class HomeView(TemplateView):
 class AddRouteView(View):
     http_method_names = ["post"]  # noqa: RUF012
 
-    def post(self, _: HttpRequest, *__: Any, **___: Any) -> HttpResponseRedirect:
+    # noinspection PyMethodMayBeStatic
+    def post(self, request: HttpRequest, *_: Any, **__: Any) -> HttpResponseRedirect:
+        if request.user.is_authenticated:
+            anchor: tuple[float, float] = float(str(request.POST["startCoords"]).split(",")[0]), float(str(request.POST["startCoords"]).split(",")[1])
+            route = LocationRoute.objects.create(user=request.user)
+            for business in BusinessRatingLocation.objects.annotate(distance=Distance("location", Point(anchor, srid=4326))).order_by("distance")[:int(request.POST["locationRange"])]:
+                route.business_rating_locations.add(business)
         return shortcuts.redirect("/my-routes")
 
 
@@ -49,6 +58,10 @@ class UserView(TemplateView):
                 for business_location
                 in route.business_rating_locations.all()
             ]
+
+            if not original_points:
+                continue
+
             context["full_routes"].append(
                 {
                     "id": route.id,
