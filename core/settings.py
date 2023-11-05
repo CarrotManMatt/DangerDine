170b3589826265c062001f888e0f4d0b98669c18
CarrotManMatt <matt@carrotmanmatt.com>
2023-11-05 11:57:43 +0000

    Remove deployment

--- core/settings.py
+++ core/settings.py
@@ -42,7 +42,7 @@ if env("PRODUCTION"):
 else:
     dev_env: Env = Env(
         DEBUG=(bool, True),
-        ALLOWED_HOSTS=(list, ["localhost", "0.0.0.0"]),
+        ALLOWED_HOSTS=(list, ["localhost"]),
         LOG_LEVEL=(str, "INFO")
     )
 
