7ae701f3bd92aa7d74812c14b57d062e9f0d9047
CarrotManMatt <matt@carrotmanmatt.com>
2023-11-05 11:00:26 +0000

    Fix lint issues

--- dangerdine/views.py
+++ dangerdine/views.py
@@ -5,16 +5,18 @@ from typing import TYPE_CHECKING, Any
 
 from django import shortcuts
 from django.contrib import auth
-from django.contrib.gis.geos import Point
 from django.contrib.gis.db.models.functions import Distance
+from django.contrib.gis.geos import Point
 from django.http import HttpRequest, HttpResponseRedirect
 from django.views import View
 from django.views.generic.base import TemplateView
 
 import dangerdine.utils
-from dangerdine.models import LocationRoute, BusinessRatingLocation
+from dangerdine.models import BusinessRatingLocation, LocationRoute
 
 if TYPE_CHECKING:
+    from django.db.models import QuerySet
+
     from dangerdine.models import User
 
 # NOTE: Adding external package functions to the global scope for frequent usage
@@ -32,10 +34,17 @@ class AddRouteView(View):
     # noinspection PyMethodMayBeStatic
     def post(self, request: HttpRequest, *_: Any, **__: Any) -> HttpResponseRedirect:
         if request.user.is_authenticated:
-            anchor: tuple[float, float] = float(str(request.POST["startCoords"]).split(",")[0]), float(str(request.POST["startCoords"]).split(",")[1])
-            print(anchor)
-            route = LocationRoute.objects.create(user=request.user, anchor=Point(anchor, srid=4326))
-            for business in BusinessRatingLocation.objects.annotate(distance=Distance("location", Point(anchor, srid=4326))).order_by("distance")[:int(request.POST["locationRange"])]:
+            anchor: tuple[float, float] = float(
+                str(request.POST["startCoords"]).split(",")[0]
+            ), float(str(request.POST["startCoords"]).split(",")[1])
+            route = LocationRoute.objects.create(
+                user=request.user,
+                anchor=Point(anchor, srid=4326)
+            )
+            all_businesses: QuerySet[BusinessRatingLocation] = BusinessRatingLocation.objects.annotate(  # noqa: E501
+                distance=Distance("location", Point(anchor, srid=4326))
+            ).order_by("distance")[:int(request.POST["locationRange"])]
+            for business in all_businesses:
                 route.business_rating_locations.add(business)
         return shortcuts.redirect("/my-routes")
 
@@ -55,7 +64,11 @@ class UserView(TemplateView):
         route: LocationRoute
         for route in user.location_routes.all():
             original_points: list[tuple[float, float, str]] = [
-                (business_location.location.x, business_location.location.y, f"{business_location.name} - {business_location.food_hygiene_rating}★")
+                (
+                    business_location.location.x,
+                    business_location.location.y,
+                    f"{business_location.name} - {business_location.food_hygiene_rating}★"
+                )
                 for business_location
                 in route.business_rating_locations.all().order_by("-id")
             ]
@@ -66,8 +79,12 @@ class UserView(TemplateView):
             context["full_routes"].append(
                 {
                     "id": route.id,
-                    "original_points": [[point[1], point[0], point[2]] for point in original_points],
-                    "route_points": dangerdine.utils.getPolyLinePoints([tuple(route.anchor)] + original_points)  # type: ignore[attr-defined]
+                    "original_points": [
+                        [point[1], point[0], point[2]] for point in original_points
+                    ],
+                    "route_points": dangerdine.utils.getPolyLinePoints(  # type: ignore[attr-defined]
+                        [tuple(route.anchor), *original_points]
+                    )
                 }
             )
         return context
