92626f937bb78d8f962d896543c268e2ac097deb
CarrotManMatt <matt@carrotmanmatt.com>
2023-11-05 01:59:30 +0000

    Add coordinates to `home.html`

--- dangerdine/views.py
+++ dangerdine/views.py
@@ -1,10 +1,12 @@
 """Views in dangerdine app."""
 
 from collections.abc import Callable
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 from django.contrib import auth
 from django.views.generic.base import TemplateView
+
+import dangerdine.utils
 
 if TYPE_CHECKING:
     from dangerdine.models import User
@@ -16,3 +18,8 @@ get_user_model: Callable[[], "User"] = auth.get_user_model  # type: ignore[assig
 class HomeView(TemplateView):
     template_name = "dangerdine/home.html"
     http_method_names = ["get"]  # noqa: RUF012
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["route_points"] = dangerdine.utils.getPolyLinePoints()  # type: ignore[attr-defined]
+        return context
